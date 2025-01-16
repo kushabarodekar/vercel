696e16f48d49d0e871e82410778b2d310b50802f
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 16:29:26 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 # api/index.py
-import json
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 import json
@@ -9,29 +8,15 @@ class handler(BaseHTTPRequestHandler):
          # Parse the URL
         url = urlparse(self.path)
         query_params = parse_qs(url.query)
-        
-        # Log query parameters
-        #print(f"Query parameters: {query_params}")
-        
 
-        #print(getMarks(query_params))
-        # Respond to the client
+        print(getMarks(names))
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        
-        # Parse the URL
-        url = urlparse(self.path)
-        query_params = parse_qs(url.query)
-
-        print(getMarks(query_params))
         self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
         return
 
     def getMarks(names):
         print(names)
-        #with open('q-vercel-python.json', 'r') as file:
-        #    data = json.load(file)
-        #print(data)
         return names
 
