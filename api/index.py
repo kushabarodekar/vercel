126f0bf4ed792535e0e1b0b0f470276addf797a3
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 18:27:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -28,7 +28,16 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
         self.wfile.write(json.dumps(output).encode('utf-8'))
         return
+        
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')  # Allow all origins
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')  # Allowed methods
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')  # Allowed headers
+        self.end_headers()
 
