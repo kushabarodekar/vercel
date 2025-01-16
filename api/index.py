4f9db483b665fe0267eacbf8db5abc18e2f92f4b
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 15:27:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
 # api/index.py
 import json
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+        
+        # Parse the URL
+        url = urlparse(self.path)
+        query_params = parse_qs(url.query)
+        
+        self.wfile.write(json.dumps(query_params).encode('utf-8'))
         return
+
