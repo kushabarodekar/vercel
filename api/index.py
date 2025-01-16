dd1cf55e1833175922bd1fc01b95da7fb89bad74
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 16:33:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,14 +5,17 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        data = None
          # Parse the URL
         url = urlparse(self.path)
         query_params = parse_qs(url.query)
 
-        print(query_params)
+        with open('q-vercel-python.json', 'r') as file:
+            data = json.load(file)
+        print(data)
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(query_params).encode('utf-8'))
+        self.wfile.write(json.dumps(data).encode('utf-8'))
         return
 
