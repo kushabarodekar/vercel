215942fe02afec5c1ee11e12360f031f38246914
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 15:37:14 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@
 import json
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -13,6 +14,11 @@ class handler(BaseHTTPRequestHandler):
         url = urlparse(self.path)
         query_params = parse_qs(url.query)
         
-        self.wfile.write(json.dumps(query_params).encode('utf-8'))
+        self.wfile.write(json.dumps(getMarksForStudent(query_params)).encode('utf-8'))
         return
+
+    def getMarksForStudent(names):
+        with open('../q-vercel-python.json', 'r') as file:
+            data = json.load(file)
+        return data
 
