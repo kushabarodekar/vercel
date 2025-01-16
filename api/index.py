e5b1f751b42bc03590a9bcbf70d4f26515b3e7b5
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-01-16 16:57:40 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,16 +6,27 @@ import json
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         data = None
+        markList = []
          # Parse the URL
         url = urlparse(self.path)
         query_params = parse_qs(url.query)
 
         with open('q-vercel-python.json', 'r') as file:
             data = json.load(file)
+
+        for key, value in query_params.items():
+            marks = None
+            for item in data:
+                if item.get("name") == value:
+                    marks = item.get("marks")
+                    markList.append(marks)
+        
+        output = {"marks":markList}
+        
         print(data)
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        self.wfile.write(json.dumps(output).encode('utf-8'))
         return
 
